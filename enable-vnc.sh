f15b8fb75917485752d16bfc214db424ff67fd4e
Vasily Pleshakov <wasa@yandex.ru>
2020-01-29 09:55:31 -0800

    export LC_CTYPE

--- enable-vnc.sh
+++ enable-vnc.sh
@@ -11,7 +11,7 @@ if [[ -z "${APPVEYOR_VNC_PASSWORD}" ]]; then
     echo "Generating one..."
     USER_PASSWORD_LENGTH=20
     echo $LC_CTYPE
-    APPVEYOR_VNC_PASSWORD=$(LC_CTYPE=C; cat /dev/urandom | tr -dc _A-Z-a-z-0-9 | head -c${USER_PASSWORD_LENGTH};)
+    APPVEYOR_VNC_PASSWORD=$(export LC_CTYPE=C; cat /dev/urandom | tr -dc _A-Z-a-z-0-9 | head -c${USER_PASSWORD_LENGTH};)
     echo $LC_CTYPE
     echo -e "Password set to ${YELLOW}'${APPVEYOR_VNC_PASSWORD}'${NC}"
 fi
